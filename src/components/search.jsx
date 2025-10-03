7902e8091010f435319ee184758b71cc6873cba5
Tenzin Lekshey <105097758+TenzinNsut@users.noreply.github.com>
2025-10-03 13:49:46 +0530

    Added On key press Event

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -5,7 +5,7 @@ function Search(props) {
   return (
       <div className='flex items-center space-x-10'>
             <input type="text" value={props.searchData} onChange={()=> props.eventHandler(searchInput.current.value)} ref={searchInput} className="capitalize p-[0.5em] outline-none text-white bg-transparent border-b-[rgba(255,255,255,0.25)] border-b-2  placeholder:text-[rgba(255,255,255,0.50)] " placeholder="Location.." />
-            <button onClick={props.searchWeather} className="bg-white px-5 py-2">Search</button>
+            <button onClick={props.searchWeather} onKeyDown={props.searchWeather} className="bg-white px-5 py-2">Search</button>
       </div>
     )
   }
